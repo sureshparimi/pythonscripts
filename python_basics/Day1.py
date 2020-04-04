8a49656ee100e85f5e42fe27e1ba3810dc718408
sureshparimi <sparimi2020@gmail.com>
2020-04-04 20:58:29 +0530

    Day1  code

---
+++ python_basics/Day1.py
@@ -0,0 +1,22 @@
+# declare int, double and string - read from stdin
+input_int = int(input())
+input_double = float(input())
+input_string = str(input())
+
+i = 4
+
+d= 5.0
+
+s = "any string"
+
+# print adding i with input_int
+
+print( i + input_int)
+
+# print adding d with input_double
+
+print( round(d + input_double,1))
+
+# print adding str with input_str
+
+print( s + input_string)

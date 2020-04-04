b5fb6527c2195a3d1573d26e8b665c5426b4237f
sureshparimi <sparimi2020@gmail.com>
2020-04-04 15:45:38 +0530

    modified code in largest.py

--- largest_in_list.py
+++ largest_in_list.py
@@ -1,1 +1,11 @@
 def largest_in_list(list):
+    largest = None
+    for current_number in list:
+        if largest == None:
+            largest = current_number
+        elif current_number > largest:
+            largest = current_number
+    return largest 
+
+result = largest_in_list([1,2,3,45,6,7,98])
+print(result)
